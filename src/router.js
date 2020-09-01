da888d64f655ebd2f15775f45285ef0505948c5a
AVICCII <863134471@qq.com>
2020-09-01 10:09:15 +0800

    2020/9/1/ first commit

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Router from 'vue-router'
 // 导入刚才编写的组件
 import AppIndex from '@/components/home/Appindex'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
+import LibraryIndex from "@/library/LibraryIndex";
 
 
 Vue.use(Router)
@@ -11,10 +13,34 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     routes: [
-        { path: '/', redirect: '/login' },
-        { path: '/login', name: 'Login', component: Login },
-        { path: '/index', name: 'AppIndex', component: AppIndex,meta:{
-            requireAuth: true
-            } }
+        {
+            path: '/home',
+            name: 'Home',
+            component: Home,
+            redirect: '/index',
+            children: [
+                {
+                    path: '/index',
+                    name: 'AppIndex',
+                    component: AppIndex,
+                    meta: {
+                        requireAuth: true
+                    }
+                },
+                {
+                    path: '/library',
+                    name: 'Library',
+                    component: LibraryIndex,
+                    meta: {
+                        requireAuth: true
+                    }
+                }
+            ]
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        }
     ]
 })
